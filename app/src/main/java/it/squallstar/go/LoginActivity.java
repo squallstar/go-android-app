2c4b1ca504fcf7ec4815ff4bfab99db1194c571d
Nicholas Valbusa <squallstar@gmail.com>
2015-07-31 15:03:47 +0100

    removed stuff

--- app/src/main/java/it/squallstar/go/LoginActivity.java
+++ app/src/main/java/it/squallstar/go/LoginActivity.java
@@ -55,29 +55,9 @@ public class LoginActivity extends Activity {
     	}
     }
 
-    private void loadMainActivity()
-    {
-    	Intent mainActivity = new Intent(this, MainActivity.class);
-		mainActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-		startActivity(mainActivity);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    private void loadMainActivity() {
+        Intent mainActivity = new Intent(this, MainActivity.class);
+        mainActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainActivity);
     }
 }
